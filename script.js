6610d250e9ca4c093e59d5ccc202d74bbe22ebfd
Jonh1788 <110953238+Jonh1788@users.noreply.github.com>
2023-04-26 17:46:14 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,90 @@
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
+
+function iniciar(){
+
+    
+    paragrafo1 = document.getElementById('iaBusca');
+    carregar = document.getElementById('carregar');
+
+    contadorMinas = document.getElementById('contadorDeMinas');
+    contadorTentativas = document.getElementById('contadorDeTentativas');
+    atencao = document.getElementById('atencao');
+    link = document.getElementById('link');
+
+    paragrafo1.classList.remove("sumir");
+    carregar.classList.remove("sumir");
+
+    setTimeout(function(){
+
+        contadorMinas.classList.remove('sumir');
+        contadorTentativas.classList.remove('sumir');
+        atencao.classList.remove('sumir');
+        carregar.classList.remove('c-loader')
+        carregar.classList.add('sumir');
+        carregar.classList.add('c-loader');
+        link.classList.remove('sumir')
+
+        var listaDeEle = document.getElementsByClassName('grid-item');
+        var listaEm = shuffle(listaDeEle);
+    
+        var imagemY = "bomba.svg";
+        var imagemX = "diamante.svg"
+    
+        var vezesImagemX = 5;
+        var contadorImagemX = 0;
+        var contador = listaDeEle.length;
+        var array = [];
+        for (var i = 0; i < 25; i++) {
+            array.push(i); // Adiciona o valor de i ao final do array
+          }
+
+        array = shuffle(array);  
+
+        for(let i = 0; i < listaEm.length; i++)
+        {
+       
+            listaEm[i].classList.remove('sumir');
+            var primeiroFilho = listaEm[i].querySelector("img:first-child");
+            if(primeiroFilho != null)
+            {
+                primeiroFilho.remove();
+            }
+    
+        }
+
+        for(let i = 0; i < listaEm.length; i++)
+        {
+            var imagem = document.createElement("img");
+    
+            if(contadorImagemX < vezesImagemX)
+            {
+               imagem.src = imagemX;
+               contadorImagemX++;
+            }else
+            {
+                imagem.src = imagemY;
+            }
+    
+            listaEm[array[i]].appendChild(imagem);
+    
+        }
+
+    }, 2000)
+
+}
